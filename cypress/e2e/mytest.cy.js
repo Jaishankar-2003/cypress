4a27bb544895dffdeb20f4d06d919e47838785a1
Jaishankar-2003 <Jaishankar98421@gmail.com>
2024-04-04 17:47:57 +0530

    file

---
+++ cypress/e2e/mytest.cy.js
@@ -0,0 +1,12 @@
+describe('test verify', () => {
+  it('test1 title positive', () => {
+    cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    cy.title().should('eq','OrangeHRM')
+  })
+
+  it('test2 title negative', () => {
+    cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    cy.title().should('eq','OrangeHRcy')
+  })
+
+})
